94affc98512489a8440ff24deb954174e6d30d98
83372 <50212258+83372@users.noreply.github.com>
2019-05-12 03:33:00 +0530

    Introduced better testing framework

--- scripts/setup-student-repo.sh
+++ scripts/setup-student-repo.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
+set -x
 
 pushd ..
 git clone --mirror "https://$GITHUB_API@github.com/$ASSIGNMENT_REPO_SLUG" $ASSIGNMENT_REPO_SLUG/.git
 pushd $ASSIGNMENT_REPO_SLUG
 git config --bool core.bare false
 popd
+
+git clone "https://$GITHUB_API@github.com/$TESTCASES_REPO_SLUG" $TESTCASES_REPO_SLUG/
+pushd $TESTCASES_REPO_SLUG
 ls -al
+npm install
+popd
+
 popd
 pwd
--- scripts/test-all-branches.sh
+++ scripts/test-all-branches.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 CURRENT_DIR=$(pwd)
 for branch in $(git  --git-dir ../../83372/$ASSIGNMENT_REPO_SLUG/.git  branch --list  | grep -v "^*" ) ; do (
+
   pushd ../../83372/$ASSIGNMENT_REPO_SLUG
+  SOURCE_PATH=$(pwd)
   git checkout $branch
   pwd
-  for d in ./*/ ; do (echo $d && node $CURRENT_DIR/scripts/require-all-files.js $(pwd)/$d); done
   popd
+
+
+  pushd ../../83372/$TESTCASES_REPO_SLUG
+  export SOURCE_PATH=$SOURCE_PATH/src/
+  ls -al
+  mocha
+  popd
+
 ); done
