f6492f8efc295fab1514a1f676ef4b7104186803
83372 <50212258+83372@users.noreply.github.com>
2019-05-12 05:46:21 +0530

    Incorporated according to web hook config

--- scripts/test-all-branches.sh
+++ scripts/test-all-branches.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 CURRENT_DIR=$(pwd)
-for branch in $(git  --git-dir ../../83372/$ASSIGNMENT_REPO_SLUG/.git  branch --list  | grep -v "^*" ) ; do (
-
-  pushd ../../83372/$ASSIGNMENT_REPO_SLUG
-  SOURCE_PATH=$(pwd)
-  git checkout $branch
-  pwd
-  popd
+pushd ../../83372/$ASSIGNMENT_REPO_SLUG
+SOURCE_PATH=$(pwd)
+git checkout $BRANCH_TO_CHECKOUT
+pwd
+popd
 
 
-  pushd ../../83372/$TESTCASES_REPO_SLUG
-  export SOURCE_PATH=$SOURCE_PATH/src/
-  ls -al
-  mocha
-  popd
-
-); done
+pushd ../../83372/$TESTCASES_REPO_SLUG
+export SOURCE_PATH=$SOURCE_PATH/src/
+ls -al
+mocha
+popd
