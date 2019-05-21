0d8ba446a650210d96188aaf5e08ba78921059ba
83372 <50212258+83372@users.noreply.github.com>
2019-05-21 22:39:14 +0530

    Fixed: Deleting issues of one comment only

--- scripts/publish-report.sh
+++ scripts/publish-report.sh
@@ -2,7 +2,7 @@
 set -x
 REPORT=$(node ./scripts/format-report.js "$TRAVIS_BUILD_NUMBER" /tmp/latest_test_report.json)
 BODY=$(jq --arg body "$REPORT" '{"body":$body}' <<< {})
-curl -X GET   -H "User-Agent: $USER_AGENT"   "https://$GITHUB_API@api.github.com/repos/83372/practice_language_basics/issues/13/comments" | jq '.[] | .id' > comments.txt
+curl -X GET   -H "User-Agent: $USER_AGENT"   "https://$GITHUB_API@api.github.com/repos/83372/practice_language_basics/issues/$ISSUE_NUMBER/comments" | jq '.[] | .id' > comments.txt
 curl -X POST -H "User-Agent: $USER_AGENT" --data "$BODY" "https://$GITHUB_API@api.github.com/repos/83372/practice_language_basics/issues/$ISSUE_NUMBER/comments" > /dev/null
 
 cat comments.txt
